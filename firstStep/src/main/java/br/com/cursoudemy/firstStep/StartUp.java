1bb5362c036b5a1147bb7d26ffe549eef4624e93
Etanielcaus <nielfilosofia@gmail.com>
2024-11-25 21:19:13 -0300

    rename application

--- firstStep/src/main/java/br/com/cursoudemy/firstStep/StartUp.java
+++ firstStep/src/main/java/br/com/cursoudemy/firstStep/StartUp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstStepApplication {
+public class StartUp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstStepApplication.class, args);
+		SpringApplication.run(StartUp.class, args);
 	}
 
 }

d5a78466c94b53075272ed4f8b5c8a07e9360bc7
Etanielcaus <nielfilosofia@gmail.com>
2024-11-25 21:14:53 -0300

    first commit

---
+++ firstStep/src/test/java/br/com/cursoudemy/firstStep/FirstStepApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cursoudemy.firstStep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstStepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

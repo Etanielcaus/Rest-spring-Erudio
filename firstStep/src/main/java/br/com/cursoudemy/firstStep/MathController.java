35a2f5025c407419dc36f4d7014a4721c30fdaa0
Etanielcaus <nielfilosofia@gmail.com>
2024-11-26 22:40:08 -0300

    61. 0604 Definindo os Métodos de Validação e Executando a Aplicação

--- firstStep/src/main/java/br/com/cursoudemy/firstStep/MathController.java
+++ firstStep/src/main/java/br/com/cursoudemy/firstStep/MathController.java
@@ -1,6 +1,8 @@
 package br.com.cursoudemy.firstStep;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -15,7 +17,7 @@ public class MathController {
             @PathVariable(value = "numberTwo") String numberTwo
             ) throws Exception {
 
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo) ){
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
             throw new Exception();
         }
 
@@ -23,11 +25,22 @@ public class MathController {
     }
 
     private Double convertToDouble(String strNumber) {
+        if (strNumber == null) return 0D;
+
+//        BR 10,25 US 10.25
+        String number = strNumber.replaceAll(",", ".");
+
+        if (isNumeric(number)) return Double.parseDouble(number);
+
         return Double.parseDouble(strNumber);
     }
 
     private boolean isNumeric(String strNumber) {
-        return false;
+        if (strNumber == null) return false;
+
+        String number = strNumber.replaceAll(",", ".");
+
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 
 }

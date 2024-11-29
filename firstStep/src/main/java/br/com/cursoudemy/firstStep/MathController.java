eee35612d04a5fb0507862588b6ce62ae4bb621e
Etanielcaus <nielfilosofia@gmail.com>
2024-11-28 21:27:01 -0300

    63. 0606 [DESAFIO] Implemente as Demais Operações Matemáticas

--- firstStep/src/main/java/br/com/cursoudemy/firstStep/MathController.java
+++ firstStep/src/main/java/br/com/cursoudemy/firstStep/MathController.java
@@ -25,6 +25,61 @@ public class MathController {
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
 
+    @RequestMapping(value = "/sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double subtraction(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws Exception {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/div/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double division(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws Exception {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/media/{numberOne}/{numberTwo}/{numberThree}", method = RequestMethod.GET)
+    public Double media(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo,
+            @PathVariable(value = "numberThree") String numberThree
+    ) throws Exception {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo) || !isNumeric(numberThree)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+
+        Double media = (convertToDouble(numberOne) + convertToDouble(numberTwo) + convertToDouble(numberThree)) / 3;
+        return media;
+    }
+
+    @RequestMapping(value = "/raiz/{number}", method = RequestMethod.GET)
+    public Double raizQuadrada(
+            @PathVariable(value = "number") String number
+    ) throws Exception {
+
+        if (!isNumeric(number)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+
+        return Math.sqrt(convertToDouble(number));
+    }
+
+
+
     private Double convertToDouble(String strNumber) {
         if (strNumber == null) return 0D;
 
